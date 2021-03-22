10c012cc333eac5dbee1f3e98636509ec29a350c
Nahian Sajjad <sajjadrocks97@gmail.com>
2021-03-22 12:17:39 +0600

    Account

---
+++ Lab 1.1/Lab 1.1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a0 = new Account("Sajjad ", "41154", 10000);
+            var a1 = new Account("Sajjad 1", "2", 20000);
+
+            a0.Deposit(1000);
+            a0.Withdraw(500);
+            a0.Transfer(a1 ,4000);
+
+            Console.WriteLine("Account Balance");
+
+            a0.ShowInfo();
+            a1.ShowInfo();
+
+            Console.WriteLine("Account Transactions List");
+
+            a0.ShowAllTransaction();
+            a1.ShowAllTransaction();
+        }
+    }
+}
