be7636a2e6afef730a3230f68c72ff22961468b4
Nahian Sajjad <sajjadrocks97@gmail.com>
2021-04-12 23:59:21 +0600

    GUI Code

---
+++ GUI Lab 3/GUI Lab 3/Database.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace GUI_Lab_3
+{
+    class Database
+    {
+        public static SqlConnection ConnectDB()
+        {
+            string connString = @"Server=LAPTOP-6R50N3SS\SQLEXPRESS;Database=model;Integrated Security=true;";
+            SqlConnection conn = new SqlConnection(connString);
+            return conn;
+        }
+    }
+}
